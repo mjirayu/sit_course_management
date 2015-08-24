19f6ffcf4075a4836205cafa7802e8f31c18e270
tostarter <tostarter@gmail.com>
2015-08-24 23:49:36 +0700

    add route for edit course plan status :cat:

--- routes/plan.js
+++ routes/plan.js
@@ -2,23 +2,63 @@ var express = require('express');
 var router = express.Router();
 var middleware = require('./../models/middleware');
 var mongoose = require('mongoose');
-var dataplan = mongoose.model('plan');
+var dataPlan = mongoose.model('plan');
 var path = require('path');
 
 router.get('/', middleware, function(req, res, next) {
-	res.redirect('/');
+	if (req.user.roles[0] == 'admin') {
+		dataPlan.find({'status': 'pending'}, function(err, collection) {
+			res.render('plan/plan-admin', {
+				datas: collection,
+			});
+		});
+	} else {
+		res.redirect('/');
+	}
+});
+
+router.get('/edit/:id', middleware, function(req, res) {
+	if (req.user.roles[0] == 'admin') {
+		dataPlan.findById(req.params.id, function(err, collection) {
+			res.render('plan/plan-edit', {
+				data: collection,
+			});
+		});
+	} else {
+		res.redirect('/');
+	}
+});
+
+router.post('/edit/:id', middleware, function(req, res) {
+	if (req.user.roles[0] == 'admin') {
+		dataPlan.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: {
+					'status': req.body.status,
+					'message': req.body.message
+				}
+			},
+			function(err, collection) {
+				if (err) {
+					console.log(err);
+				}
+			}
+		);
+	}
+	res.redirect('/plan');
 });
 
 router.get('/:id', middleware, function(req, res) {
-	dataplan.findById(req.params.id, function(err,collection) {
+	dataPlan.findById(req.params.id, function(err, collection) {
 		res.sendfile(path.resolve('public/app.html'));
 	});
 });
 
-router.get('/api/:id', middleware, function(req, res, next) {
-	dataplan.findById(req.params.id, function(err,collection) {
-		res.json(collection);
-	});
-});
+// router.get('/api/:id', middleware, function(req, res, next) {
+// 	dataplan.findById(req.params.id, function(err,collection) {
+// 		res.json(collection);
+// 	});
+// });
 
 module.exports = router;
