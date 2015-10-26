d773afd184e4b156b47b151be7a050facbc211a7
tostarter <tostarter@gmail.com>
2015-10-26 20:20:55 +0700

    change model of plan :cat:

--- routes/plan.js
+++ routes/plan.js
@@ -1,68 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
 var auth = require('./../middlewares/auth');
 var dataPlan = require('./../models/plan');
 var dataCourse = require('./../models/course');
 
-
-router.get('/', auth, function(req, res, next) {
-	if (req.user.roles[0] == 'admin') {
-		dataPlan.find({'status': 'pending'}, function(err, collection) {
-			res.render('plan/plan-admin', {
-				datas: collection,
-			});
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-router.get('/edit/:id', auth, function(req, res) {
-	if (req.user.roles[0] == 'admin') {
-		dataPlan.findById(req.params.id, function(err, collection) {
-			res.render('plan/plan-edit', {
-				data: collection,
-			});
-		});
-	} else {
-		res.redirect('/');
-	}
-});
-
-router.post('/edit/:id', auth, function(req, res) {
-	if (req.user.roles[0] == 'admin') {
-		dataPlan.findByIdAndUpdate(
-			req.params.id,
-			{
-				$set: {
-					'status': req.body.status,
-					'message': req.body.message
-				}
-			},
-			function(err, collection) {
-				if (err) {
-					console.log(err);
-				}
-			}
-		);
-	}
-	res.redirect('/plan');
-});
-
-router.get('/:id', auth, function(req, res) {
-	dataCourse.find({}, function(err, collection) {
-		console.log(collection);
-		res.render('course-dnd',{plan:collection});
-		//res.sendfile(path.resolve('public/app.html'));
+router.get('/', function(req, res, next) {
+	dataPlan.find({}, function(err, collection) {
+		res.render('plan/plan-admin', {datas: collection});
 	});
 });
 
-// router.get('/api/:id', middleware, function(req, res, next) {
-// 	dataplan.findById(req.params.id, function(err,collection) {
-// 		res.json(collection);
-// 	});
-// });
+router.get('/edit/:id', function(req, res) {
+});
+
+router.post('/edit/:id', function(req, res) {
+});
+
+router.get('/:id', function(req, res) {
+
+});
 
 module.exports = router;
