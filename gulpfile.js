6dec413b47cd3c4a7253017023d4f28dfdbe45c8
tostarter <tostarter@gmail.com>
2015-10-26 20:20:03 +0700

    add more gitignore item and reindent on gulpfile :cat:

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default',['node']);
+gulp.task('default', ['node']);
 
-gulp.task('node',function(){
+gulp.task('node', function() {
 	nodemon({
-    script: 'bin/www',
-  	ext: 'js html',
-  	env: { 'NODE_ENV': 'development' }
-  });
+		script: 'bin/www',
+		ext: 'js html',
+		env: {'NODE_ENV': 'development'},
+	});
 });
