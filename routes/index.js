c66251b79851bcc21b0f84f744c4a28924d2ee7c
tostarter <tostarter@gmail.com>
2015-11-08 17:20:41 +0700

    refactor file :cat:

--- routes/index.js
+++ routes/index.js
@@ -9,45 +9,33 @@ var passport = require('./../middlewares/passport');
 var dataPlan = require('./../models/plan');
 var dataUser = require('./../models/user_profile');
 
-
-
-router.get('/', auth, function(req,res){
-	console.log(req.user);
-	res.render('./dnd/dnd',req.user);
-});
-
-router.get('/logout',function(req,res){
-	req.logout();
-	res.redirect('/');
-});
-
-router.get('/login',function(req, res){
-	if(req.user){
-		res.redirect('/');
-	}else{
-		res.render('login');
+router.get('/', auth, function(req, res) {
+  console.log(req.user);
+  if (req.user.reset_password != '') {
+    res.render('account/reset_password');
 	}
 
+  res.render('./dnd/dnd', req.user);
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/login', function(req, res) {
+  if (req.user) {
+    res.redirect('/');
+  } else {
+    res.render('login');
+	}
 });
 
 router.post('/login', passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/login',
-		failureFlash: true,
-	}
-));
-
-router.get('/csv', function(req, res) {
-	res.render('csv');
-});
-
-router.get('/test', function(req, res) {
-	dataUser
-		.findOne({})
-		.populate('auth')
-		.exec(function(err, data) {
-			res.json(data);
-		});
-});
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+	})
+);
 
 module.exports = router;
